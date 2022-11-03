0128ddbc6116fac58099c76d4740094eb062efb5
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-11-03 19:52:25 +0900

    database

--- src/main/java/com/ucamp/JM/service/UserServiceImpl.java
+++ src/main/java/com/ucamp/JM/service/UserServiceImpl.java
@@ -1,7 +1,22 @@
 package com.ucamp.JM.service;
 
+import com.ucamp.JM.dao.UserDAO;
+import com.ucamp.JM.dto.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
-public class UserServiceImpl {
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+
+    @Override
+    public ArrayList<User> selectUser() {
+        return userDAO.selectAll();
+    }
+
+
 }
