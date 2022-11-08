f9036acb93e3a8f407749035792615b7acad08a1
ssy <tad05@naver.com>
2022-11-08 19:46:30 +0900

    table data read & insert job
    music table 데이터 값을 읽고 다른 테이블에 작성

--- src/main/java/com/ucamp/JM/controller/RootController.java
+++ src/main/java/com/ucamp/JM/controller/RootController.java
@@ -1,14 +1,27 @@
 package com.ucamp.JM.controller;
 
+import com.ucamp.JM.service.MusicServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@AllArgsConstructor
 @RequestMapping("/")
 public class RootController {
+    private final MusicServiceImpl musicService;
+
     @GetMapping("/")
-    public String main(){
+    public String main() {
         return "main";
+    }
+
+    @GetMapping("/rank")
+    public String rank(Model model) {
+        model.addAttribute("ranklist", musicService.selectRankMusic());
+        return "rank";
     }
 
 //    @GetMapping("/loginForm")
---
+++ src/main/java/com/ucamp/JM/dto/Music.java
@@ -0,0 +1,21 @@
+package com.ucamp.JM.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder(builderMethodName = "Music")
+public class Music {
+
+    private int music_number;
+    private String music_title;
+    private String music_singer;
+    private String music_genre;
+    private String music_release;
+    private String music_image;
+    private String music_file;
+    private String music_lyrics;
+    private int music_like;
+}
