e2c0de9400f1b1dc08f0104eda90ec87c1f2718f
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-11-08 17:52:21 +0900

    회원탈퇴및 이메일 중복확인 추가

--- src/main/java/com/ucamp/JM/controller/RootController.java
+++ src/main/java/com/ucamp/JM/controller/RootController.java
@@ -28,9 +28,11 @@ public class RootController {
         return "registerform";
     }
 
+    // 용식 :회원가입
     @PostMapping("/register")
     public String register(@ModelAttribute User user) {
         try {
+            // 회원가입시 유저타입(admin or user) user 로 설정
             user.setType("user");
             userService.register(user);
         } catch (Exception e) {
@@ -44,6 +46,7 @@ public class RootController {
         return "loginform";
     }
 
+    // 용식 : 로그인
     @PostMapping("login")
     public String login(@RequestParam String user_email, @RequestParam String user_password, Model model) {
         try {
@@ -58,5 +61,47 @@ public class RootController {
         }
     }
 
+    // 용식 : 로그아웃
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    // 용식 : 이메일 중복확인
+    @ResponseBody
+    @PostMapping("/emailoverlap")
+    public boolean emailoverlap(@RequestParam String user_email) {
+        boolean result = false;
+        try {
+            // 중복된 이메일이 있으면 result = true
+            result = userService.emailOverLapCheck(user_email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    // 용식 : 회원탈퇴
+    @ResponseBody
+    @PostMapping("/deleteuser")
+    public String deleteuser(@RequestParam String user_email, @RequestParam String user_password) {
+        try {
+            User user = userService.queryUser(user_email);
+            if (user == null) {
+                return "시용자없음";
+            }
+            if (!(user.getUser_password().equals(user_password))) {
+                // 비밀번호 틀리면 삭제실패
+                return "삭제실패";
+            } else {
+                userService.deleteUser(user_email, user_password);
+                return "삭제성공";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "에러";
+        }
+    }
 }
 
