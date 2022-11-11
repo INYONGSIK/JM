b5bada6276240abd5ebac7ec023ffe3e2c10ae92
Hyunho <ghgh9467@naver.com>
2022-11-11 18:24:42 +0900

    관리자 페이지 들어가는 권한 설정 및 회원 탈퇴 기능 추가

--- src/main/java/com/ucamp/JM/controller/AdminController.java
+++ src/main/java/com/ucamp/JM/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.ucamp.JM.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,25 +16,75 @@ public class AdminController {
 
     @Autowired
     AdminService adminService;
-    Autowired
+    @Autowired
     UserService userService;
-    @RequestMapping("/admin/userList")
-    public String adminUserList(HttpServletRequest request, Model model) throws Exception {
-        String email= (String) request.getSession().getAttribute("user_email");
-        User user = userService.queryUser(email);
+
+    @RequestMapping("/admin")
+    public String admin(HttpServletRequest request) throws Exception {
+        String email = (String) request.getSession().getAttribute("user_email");
+        User user = null;
         if (email == null) {
             //loginform 으로 이동
-            return "<script>location.href='/loginform';</script>";
+            return "loginform";
+        } else {
+            user = userService.queryUser(email);
         }
 
         if (!user.getType().equals("admin")) {
-            return "<script>alert('관리자만 접속 가능합니다.'); location.href='/loginform';</script>";
-        }else{
-            model.addAttribute("Users", adminService.selectAllUser());
+            return "redirect:/";
         }
 
+        return "/admin/adminHome";
 
+    }
+
+    @RequestMapping("/admin/report/{type}")
+    public String adminReport(HttpServletRequest request, Model model, @PathVariable int type) throws Exception {
+
+        String email = (String) request.getSession().getAttribute("user_email");
+        User user = null;
+        if (email == null) {
+            //loginform 으로 이동
+            return "loginform";
+        } else {
+            user = userService.queryUser(email);
+        }
+
+        if (!user.getType().equals("admin")) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("reports", adminService.selectReportByType(type));
+        return "/admin/report";
+    }
+
+
+    @RequestMapping("/admin/userList")
+    public String adminUserList(HttpServletRequest request, Model model) throws Exception {
+
+        String email = (String) request.getSession().getAttribute("user_email");
+        User user = null;
+        if (email == null) {
+            //loginform 으로 이동
+            return "loginform";
+        } else {
+            user = userService.queryUser(email);
+        }
+
+        if (!user.getType().equals("admin")) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("users", adminService.selectAllUser());
 
         return "/admin/userList";
     }
+
+    @RequestMapping("/admin/deleteUser/{user_number}")
+    public String deleteUser(@PathVariable int user_number) {
+        adminService.deleteUser(user_number);
+        
+        return "redirect:/admin/userList";
+    }
+
 }
--- src/main/java/com/ucamp/JM/service/AdminService.java
+++ src/main/java/com/ucamp/JM/service/AdminService.java
@@ -13,7 +13,10 @@ public interface AdminService {
     //admin 회원관리 페이지에서 보여지는 회원 조회       --이현호
     ArrayList<Admin> selectAllUser();
 
-    
+    //report 관련 항목 가져오기
+    ArrayList<Report> selectAllReport();
+
+
     //이현호
     //admin 신고관리 페이지에서 받는 댓글(report_type=1), 음원(report_type=2)관련 정보
     ArrayList<Report> selectReportByType(int report_type);
