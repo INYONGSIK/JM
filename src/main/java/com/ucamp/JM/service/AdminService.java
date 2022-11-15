8a08be26570d2157d011b58bee7c90659a7b79b7
Hyunho <ghgh9467@naver.com>
2022-11-15 15:33:44 +0900

    관리자 페이지 수정

--- src/main/java/com/ucamp/JM/service/AdminService.java
+++ src/main/java/com/ucamp/JM/service/AdminService.java
@@ -2,6 +2,7 @@ package com.ucamp.JM.service;
 
 import com.ucamp.JM.dto.Admin;
 import com.ucamp.JM.dto.Report;
+import com.ucamp.JM.dto.User;
 
 import java.util.ArrayList;
 
@@ -10,11 +11,17 @@ public interface AdminService {
     //회원넘버 받아와서 회원 삭제       --이현호
     void deleteUser(int user_number);
 
+    //조치 완료된 신고 내역 삭제
+    void deleteReport(int report_ID);
+
     //admin 회원관리 페이지에서 보여지는 회원 조회       --이현호
-    ArrayList<Admin> selectAllUser();
+    ArrayList<Admin> selectAllUserByReport();
 
     //report 관련 항목 가져오기
     ArrayList<Report> selectAllReport();
+
+
+    ArrayList<User> selectAllUser();
 
 
     //이현호
